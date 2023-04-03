88127ae356eb03a0d6449995a3996b07f97acca3
santhiyasanthiya <santhiya30032@gmail.com>
2023-04-03 17:11:36 +0530

    icon

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./home.css"
 import smoke from "./Assest/smoke.mp4"
+import profile from "./Assest/profile.png"
 
 function Home() {
   return (
     <div className='home_body'>
-<section>
+<section className='profile'>
+<img className='logo' src={profile} />
     {/* <video src={smoke} autoplay muted></video> */}
     {/* <video src="smoke.mp4" autoplay muted></video> */}
     <h1>  
